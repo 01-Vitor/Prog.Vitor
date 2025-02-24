df58a7c8729a913a184061f4312f390191c7a6a4
01-Vitor <prog.vitorr@gmail.com>
2025-02-24 00:29:02 +0000

    Nivel Novato - Criação de 2 cartas.

---
+++ Desafio Super Trunfo/Novato/Novato.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+
+int main() {
+
+    //Criando duas cartas do desaio SuperTrunfo - Paises
+
+    // Declarando as variáveis
+    int populacao, populacao2;
+    int pontos_turisticos, pontos_turisticos2;
+    float PIB, PIB2;
+    float area, area2;
+    float densidade, densidade2;
+    float pib_per_capita, pib_per_capita2;
+    char cidade[50], cidade2[50];
+    char estado, estado2;
+    char codigocarta1[4], codigocarta2[4];
+
+                                                            // Criação da Carta 1
+    printf("--- Criando a carta 1 ---\n");
+
+    printf("Escolha uma letra para representar o estado (A-H): "); //Solicitando ao usuário que digite uma letra de A até H para representar o estado
+    scanf(" %c", &estado);  //Lendo a informação inserida pelo usuário referente a escolha da letra do estado
+
+    printf("Digite o código da carta (ex: A01, A02...): "); //Solicitando ao usuário que digite o código da carta
+    scanf("%s", codigocarta1); //Lendo a informação inserida pelo usuário referente a escolha do código da carta
+
+    printf("Digite o nome da cidade: "); //Solicitando ao usuário que digite o nome da cidade
+    scanf(" %[^\n]", cidade); //Lendo a informação inserida pelo usuário referente a escolha do nome da cidade
+
+    printf("Digite a população da cidade: "); //Solicitando ao usuário que digite a população da cidade
+    scanf("%d", &populacao); //Lendo a informação inserida pelo usuário referente a escolha da população da cidade
+
+    printf("Digite a quantidade de pontos turísticos: "); //Solicitando ao usuário que digite a quantidade de pontos turísticos
+    scanf("%d", &pontos_turisticos); //Lendo a informação inserida pelo usuário referente a escolha da quantidade de pontos turísticos
+
+    printf("Digite o PIB da cidade: "); //Solicitando ao usuário que digite o PIB da cidade
+    scanf("%f", &PIB); //Lendo a informação inserida pelo usuário referente a escolha do PIB da cidade
+
+    printf("Digite a área da cidade: "); //Solicitando ao usuário que digite a área da cidade
+    scanf("%f", &area); //Lendo a informação inserida pelo usuário referente a escolha da área da cidade
+
+    printf("\n");
+
+                                                            //Criação da Carta 2
+
+    printf(" --- Criando a carta 2 ---\n");
+
+    printf("Escolha uma letra para representar o estado (A-H): ");
+    scanf(" %c", &estado2);
+
+    printf("Digite o código da carta (ex: A01, A02...): ");
+    scanf("%s", codigocarta2);
+
+    printf("Digite o nome da cidade: ");
+    scanf(" %[^\n]", cidade2);
+
+    printf("Digite a população da cidade: ");
+    scanf("%d", &populacao2);
+
+    printf("Digite a quantidade de pontos turísticos: ");
+    scanf("%d", &pontos_turisticos2);
+
+    printf("Digite o PIB da cidade: ");
+    scanf("%f", &PIB2);
+
+    printf("Digite a área da cidade: ");
+    scanf("%f", &area2);
+
+    
+}
